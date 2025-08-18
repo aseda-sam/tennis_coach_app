6c4f913e4f92ff86cd15287293748f728f4e0198
Aseda Sam <aoheneadu23@gmail.com>
2025-08-18 20:42:22 +0100

    style: fix import order in alembic migration file

--- backend/alembic/versions/0c2b5d53ba8c_initial_schema.py
+++ backend/alembic/versions/0c2b5d53ba8c_initial_schema.py
@@ -8,9 +8,9 @@ Create Date: 2025-08-18 19:07:42.601149
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "0c2b5d53ba8c"
